2a5e8ead63f83b78870afb1c63b89ef3ef453844
BranK179 <2410292@dlu.edu.vn>
2026-04-22 12:54:10 +0700

    Update playlists.js

--- src/data/playlists.js
+++ src/data/playlists.js
@@ -3,49 +3,49 @@ export const allPlaylists = [
     id: "sontung",
     name: "Sơn Tùng M-TP Mix",
     description: "Tuyển tập những bài hát hay nhất",
-    coverImage: "$public/image/son-tung.jpg",
+    coverImage: "./image/son-tung.jpg",
     songs: [
       { 
         title: "Chúng Ta Của Hiện Tại", 
         artist: "Sơn Tùng M-TP", 
         src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077112/chung-ta-cua-hien-tai_ag6p0h.mp3", 
-        cover: "$public/image/son-tung.jpg",
+        cover: "./image/son-tung.jpg",
         lyrics: "Mùa thu mang giấc mơ quay về\nVẫn nguyên vẹn như hôm nào\nLá bay theo gió xôn xao\nChốn xưa anh chờ..."
       },
-      { title: "Muộn Rồi Mà Sao Còn", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077246/muon-roi-sao-ma-con_efuwqp.mp3", cover: "$public/image/son-tung.jpg"},
-      { title: "Nơi Này Có Anh", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077632/noi-nay-co-anh_mrzc22.mp3", cover: "$public/image/son-tung.jpg"},
-      { title: "Đừng Làm Trái Tim Anh Đau", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077659/son-tung-mtp-Dunglamtraitimanhdau_iuf6kb.mp3", cover: "$public/image/son-tung.jpg"},
+      { title: "Muộn Rồi Mà Sao Còn", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077246/muon-roi-sao-ma-con_efuwqp.mp3", cover: "./image/son-tung.jpg"},
+      { title: "Nơi Này Có Anh", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077632/noi-nay-co-anh_mrzc22.mp3", cover: "./image/son-tung.jpg"},
+      { title: "Đừng Làm Trái Tim Anh Đau", artist: "Sơn Tùng M-TP", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776077659/son-tung-mtp-Dunglamtraitimanhdau_iuf6kb.mp3", cover: "./image/son-tung.jpg"},
     ]
   },
   {
     id: "hieuthuhai",
     name: "HIEUTHUHAI Top Hits",
     description: "Nhạc suy hay nhạc quẩy đều có đủ",
-    coverImage: "/image/hieu-thu-hai.jpg",
+    coverImage: "./image/hieu-thu-hai.jpg",
     songs: [
       { 
         title: "Ngủ Một Mình", 
         artist: "HIEUTHUHAI", 
         src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078799/ngu-mot-minh_p0poia.mp3", 
-        cover: "/image/hieu-thu-hai.jpg",
+        cover: "image/hieu-thu-hai.jpg",
         lyrics: "Baby anh không muốn phải ngủ một mình đêm nay..."
       },
-      { title: "Không Thể Say", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078567/khong-the-say_uw5ll2.mp3", cover: "/image/hieu-thu-hai.jpg" },
-      { title: "Người im lặng gặp người hay nói", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078850/nguoi-im-lang-gap-nguoi-hay-noi_sbb17g.mp3", cover: "/image/hieu-thu-hai.jpg" },
-      { title: "Nghe Như Tình Yêu", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078828/nghe-nhu-tinh-yeu_bvwubg.mp3", cover: "/image/hieu-thu-hai.jpg"},
+      { title: "Không Thể Say", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078567/khong-the-say_uw5ll2.mp3", cover: "./image/hieu-thu-hai.jpg" },
+      { title: "Người im lặng gặp người hay nói", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078850/nguoi-im-lang-gap-nguoi-hay-noi_sbb17g.mp3", cover: "./image/hieu-thu-hai.jpg" },
+      { title: "Nghe Như Tình Yêu", artist: "HIEUTHUHAI", src: "https://res.cloudinary.com/dzisjnhfj/video/upload/v1776078828/nghe-nhu-tinh-yeu_bvwubg.mp3", cover: "./image/hieu-thu-hai.jpg"},
     ]
   },
   {
     id: "manta",
     name: "Vanatice - Manta",
     description: "",
-    coverImage: "/image/manta.jpg",
+    coverImage: "./image/manta.jpg",
     songs: [
       {
         title: "Manta",
         artist: "Vanatice",
         src: "https://res.cloudinary.com/dbokviwbv/video/upload/v1776532080/Manta_pfqba5.mp3",
-        cover: "/image/manta.jpg",
+        cover: "./image/manta.jpg",
         lyrics: ""
       },
     ]
@@ -54,13 +54,13 @@ export const allPlaylists = [
     id: "foward-motion",
     name: "Glacier - Foward Motion",
     description: "",
-    coverImage: "/image/foward-motion.jpg",
+    coverImage: "./image/foward-motion.jpg",
     songs: [
       {
         title: "Foward Motion",
         artist: "Glacier",
         src: "https://res.cloudinary.com/dbokviwbv/video/upload/v1776533684/Forward_Motion_j6bguc.mp3",
-        cover: "/image/foward-motion.jpg",
+        cover: "./image/foward-motion.jpg",
         lyrics: ""
       },
     ]
@@ -114,13 +114,13 @@ export const allPlaylists = [
     id: "suit-up",
     name: "Distayne - Suit Up",
     description: "",
-    coverImage: "/image/exobolt010.jpg",
+    coverImage: "./image/exobolt010.jpg",
     songs: [
       {
         title: "Suit Up",
         artist: "Distayne",
         src: "https://res.cloudinary.com/dbokviwbv/video/upload/v1776534465/Suit_Up-1711141651_pijepp.mp3",
-        cover: "/image/exobolt010.jpg",
+        cover: "./image/exobolt010.jpg",
         lyrics: ""
       },
     ]
